7ac9dd88e1a2638fda0766d1693fae054c47c441
Witherbear <81361869+Witherbear@users.noreply.github.com>
2023-03-17 18:20:46 -0300

    IO game platforms

---
+++ js/donutscript.js
@@ -0,0 +1,17 @@
+  const converter = new showdown.Converter();
+
+      const xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            const markdown = xhr.responseText;
+            const html = converter.makeHtml(markdown);
+            const container = document.getElementById('markdown-container');
+            container.innerHTML = html;
+          } else {
+            console.log('Error: ' + xhr.status);
+          }
+        }
+      };
+      xhr.open('GET', '/docs/pages/donutscript.md', true);
+      xhr.send(null);
